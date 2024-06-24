80ebfa12e1fa73bb24d0c31b0c22a2351d12db61
malli031998 <vvdmallikarjunareddy.com>
2024-06-24 11:46:01 +0530

    created EmployeeRepository with extends Jpa repository

--- src/main/java/com/example/repository/EmployeeRepository.java
+++ src/main/java/com/example/repository/EmployeeRepository.java
@@ -1,5 +1,9 @@
 package com.example.repository;
 
-public interface EmployeeRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.entity.Employee;
+
+public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
 
 }
